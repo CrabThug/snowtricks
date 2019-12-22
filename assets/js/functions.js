feaab3bd6ab91aac951083719f099f7086606bf0
Crabthug <damien.millet@nasm.fr>
2019-12-22 22:23:44 +0100

    ⬆️change dependance to bootstrap

--- assets/js/functions.js
+++
@@ -1,9 +0,0 @@
-function navBar() {
-    var x = document.getElementById("navMenu");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    }
-    else {
-        x.className = x.className.replace(" w3-show", "");
-    }
-}
