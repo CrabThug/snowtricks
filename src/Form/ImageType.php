4153d339495f047771a8cce94bdd661bea686b6b
CrabThug <damien.millet@nasm.fr>
2020-02-12 10:12:52 +0100

    ✨ add checkbox for primary image

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Image;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -20,6 +21,10 @@ class ImageType extends AbstractType
                     'placeholder' => "Nom alternatif",
                 ],
                 'label' => FALSE,
+            ])
+            ->add('bool', CheckboxType::class, [
+                'label'    => 'Image principale',
+                'required' => false,
             ])
             ->add('file', FileType::class, [
                 'label' => FALSE,
